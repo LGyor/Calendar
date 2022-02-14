700b52ec7f470998f4f4188b3b2aef2b500e806c
Gyor Laszlo <lgyor700803@gmail.com>
2022-02-14 17:19:59 +0100

    introduction head

--- calendar.js
+++ calendar.js
@@ -3,7 +3,7 @@
 window.onscroll = function() {firstPage()}
 
 function firstPage(){
-	let topButton = document.getElementById("top_button");	
+	let topButton = document.getElementById("jumptop_button");	
 	let verticalPosition = document.documentElement.scrollTop;
 
 	/*console.log(`Scroll vertical: ${verticalPosition}`);*/
@@ -11,12 +11,12 @@ function firstPage(){
 	if (verticalPosition > 500){
 	  /*topButton.style.backgroundColor ="blue";*/
 	  /*topButton.style.visibility ="visible";*/
-	  document.getElementById("top_button").classList.add("showup");
+	  document.getElementById("jumptop_button").classList.add("showup");
 
 	}else{
 	  /*topButton.style.backgroundColor ="white";*/ 
 	  /*topButton.style.visibility ="hidden"; */ 
-	  document.getElementById("top_button").classList.remove("showup"); 
+	  document.getElementById("jumptop_button").classList.remove("showup"); 
 	}
 }
 
